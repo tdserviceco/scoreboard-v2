e1011e4ae8b04fbfdb434987395cee055f9cc581
Tommy Danielsson <tdserviceco@gmail.com>
2020-10-04 20:19:02 +0200

    Debug continue

--- src/Components/Scores.jsx
+++ src/Components/Scores.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import "./ScoreHandler.css";
+import io from "socket.io-client";
+let replaceScoreP1 = 0,
+  replaceScoreP2 = 0;
+
 class Scores extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scoreP1: replaceScoreP1,
+      scoreP2: replaceScoreP2,
+      player: "player-" + props.player
+    };
+  }
   render() {
     return (
-      <div>
-        hello
-      </div>
+      <>
+      </>
     );
   }
 }
