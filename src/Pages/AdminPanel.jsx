019e1a9c119b32a3353fb55a66e0bb6c9aff776e
Tommy Danielsson <tdserviceco@gmail.com>
2020-10-04 20:06:46 +0200

    Debug continue

--- src/Pages/AdminPanel.jsx
+++ src/Pages/AdminPanel.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import SelectPlayer from '../Components/SelectPlayer';
-import ScoreHandler from '../Components/ScoreHandler';
+import Scores from '../Components/Scores';
 class AdminPanel extends Component {
   render() {
     return (
       <div className="admin-panel">
         <div className="P1-settings">
           <SelectPlayer player="1" />
-          <ScoreHandler player="1" />
+          <Scores player="1" />
         </div>
       </div>
     );
