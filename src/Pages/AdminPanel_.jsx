54ac5e178e371106df7a540f7e448c5617673e60
Tommy Danielsson <tdserviceco@gmail.com>
2020-10-04 19:39:42 +0200

    Found the issue now I need to fix the AdminPanel issue

---
+++ src/Pages/AdminPanel_.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import SelectPlayer from '../Components/SelectPlayer';
+import ScoreHandler from '../Components/ScoreHandler';
+import SwitchPlace from '../Components/SwitchPlace';
+import "./AdminPanel.css";
+class AdminPanel extends Component {
+
+  render() {
+    return (
+      <div className="admin-panel">
+        <div className="P1-settings">
+          <SelectPlayer player="1" />
+          <ScoreHandler player="1" />
+        </div>
+        <div className="Switch-me">
+          <SwitchPlace />
+        </div>
+        <div className="P2-settings">
+          <SelectPlayer player="2" />
+          <ScoreHandler player="2" />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default AdminPanel;
